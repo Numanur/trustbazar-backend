75bc5f47b9dcc66a241edfb1b6842eec4be9a35b
Numanur <116955339+Numanur@users.noreply.github.com>
2025-07-19 02:36:33 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 // const app = require("./app");
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+
 // external import
 require("dotenv").config();
 const express = require("express");
