5d6380dd586fe86d1f6ff1cb9a48511d9dcfb423
Numanur <116955339+Numanur@users.noreply.github.com>
2025-07-19 02:09:58 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,7 +15,12 @@ const app = express();
 
 // middlewares
 app.use(morgan("dev"));
-app.use(cors());
+
+app.use(cors({
+  origin: "*",
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
