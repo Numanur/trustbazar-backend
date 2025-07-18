7245e81ef20cafe9476cd70725ff45bcc2b61ecb
Numanur <116955339+Numanur@users.noreply.github.com>
2025-07-19 01:00:29 +0600

    Delete client directory

--- client/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Index from "./routes";
-
-const App = () => {
-  return <Index />;
-};
-
-export default App;
--- client/src/utils/makeRequest.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// const REACT_APP_API_URL = 'http://localhost:5000/api';
-// const REACT_APP_API_URL = 'https://blockchain-project-api.vercel.app/api';
-const REACT_APP_API_URL = "https://trustbazar-backend.onrender.com";
-
-export const publicRequest = axios.create({
-  baseURL: REACT_APP_API_URL,
-});
